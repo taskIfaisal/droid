0f36854db8f87401579b9f27a75f08a8547fdca6
faisal.io <50693350+taskIfaisal@users.noreply.github.com>
2026-03-15 21:47:10 +0700

    Create sw-card2magic.js

---
+++ sw-card2magic.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'card2magic-v1';
+const urlsToCache = [
+    './',
+    './card2magic.html',
+    './manifest-card2magic.json',
+    './icon/card2magic-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
